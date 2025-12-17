a5f8fc2b3d35671faa47dc47acb73e267d4f9b78
ern427 <erenarsat@gmail.com>
2025-12-17 20:19:02 +0300

    Fix firebase api key

--- firebase.js
+++ firebase.js
@@ -1,19 +1,19 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDRRQdUh-lw-dSGdKeNswZHaZ5tcOld0",
+  apiKey: "BURAYA_YENİ_KOPYALADIĞIN_API_KEY",
   authDomain: "ern4-site.firebaseapp.com",
   projectId: "ern4-site",
-  storageBucket: "ern4-site.firebasestorage.app",
-  messagingSenderId: "169213127920",
-  appId: "1:169213127920:web:534afce822a4fe85706635"
+  storageBucket: "ern4-site.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "1:XXXX:web:XXXX"
 };
 
-// Init
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
